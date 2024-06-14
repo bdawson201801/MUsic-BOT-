34659f70e03a3dbfe062f488eb792b5a55a6d2b3
bdawson201801 <82557172+bdawson201801@users.noreply.github.com>
2024-06-14 02:39:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ app.get('/', (req, res) => {
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Listening to Mide music beta bot : http://localhost:${port}`);
 });
 
 printWatermark();
